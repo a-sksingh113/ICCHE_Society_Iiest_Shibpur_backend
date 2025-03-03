b2dbfa1d0aff240762b5dd23fb63247092bdfb26
Satish Singh <i.sksingh113@gmail.com>
2025-03-04 01:38:00 +0530

    6th commit

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,10 @@ const express = require('express')
 const connectDB = require('./config/dbConnection');
 const cookieParser = require('cookie-parser');
 
-const adminsRoute = require('./routes/adminsRoute')
-
-
+const adminsRoute = require('./routes/adminsRoute');
+const studentRoute = require('./routes/studentRoute');
+const volunteerRoute = require('./routes/volunteerRoute');
+const alumniRoute = require('./routes/alumniRoute')
 
 const app = express();
 PORT = process.env.PORT || 8001;
@@ -18,8 +19,6 @@ app.use('/api/admin',adminsRoute);
 app.use('/api/students',studentRoute);
 app.use('/api/volunteers',volunteerRoute);
 app.use('/api/alumni',alumniRoute);
-
-
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
