a0e12fc75c22edf1a812ea7faf15feae1687e9e8
ShaluKasera <shalukumari93129@gmail.com>
2025-03-28 23:12:21 +0530

    fix footer

--- frontend/src/components/pages/AboutUs/About.jsx
+++ frontend/src/components/pages/AboutUs/About.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Layout from '../../layout/Layout'
+import React from "react";
+import Layout from "../../layout/Layout";
 
 const About = () => {
   return (
     <Layout>
-        about
+      <div>About our society</div>
     </Layout>
-  )
-}
+  );
+};
 
-export default About
+export default About;
