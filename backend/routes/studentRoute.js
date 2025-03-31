ebe10fb58e97210f4da7c9efdba7ca23ed75baaa
Satish Singh <i.sksingh113@gmail.com>
2025-03-31 14:49:29 +0530

    42th commit

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const { getAllStudents, getStudentById } = require("../controllers/studentController");
+const { getAllStudents, getStudentById} = require("../controllers/studentController");
 const router = express.Router();
 router.get("/", getAllStudents);
 router.get("/:id", getStudentById);
-
 module.exports = router;
 
