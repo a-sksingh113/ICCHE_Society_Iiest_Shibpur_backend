9bb06307d7d47ad64ec31349899ebc402f6d9462
Satish Singh <i.sksingh113@gmail.com>
2025-04-12 17:38:17 +0530

    60th commit

--- backend/models/alumniModel.js
+++ backend/models/alumniModel.js
@@ -7,18 +7,14 @@ const alumniSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true,
-        unique: true,
         trim: true
     },
     contactNumber: {
         type: String,
-        required: true,
         trim: true
     },
     enrollmentNo:{
         type:String,
-        required:true,
         unique:true
     },
     gender: {
