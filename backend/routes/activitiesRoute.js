45578781c97559b7d3c95db0d6774a2d0227ce2d
Satish Singh <i.sksingh113@gmail.com>
2025-04-24 05:02:56 +0530

    redis implement

--- backend/routes/activitiesRoute.js
+++ backend/routes/activitiesRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { getAllActivities,  getAllPrograms, getProgramById, getActivityById} = require("../controllers/activitiesController");
+const cache = require('../middleware/redisMidlleware');
 const router = express.Router();
 
 //for activity like sports day, drawing competetion
-router.get("/activities", getAllActivities);
+router.get("/activities",cache(600), getAllActivities);
 router.get("/activities/:id", getActivityById);
 // for programs in activity like tug of war, musical chair in sports day
 router.get("/activities/:id/programs", getAllPrograms);
--- backend/routes/classroomRoute.js
+++ backend/routes/classroomRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getAllClassrooms, getClassroomById } = require("../controllers/classroomController");
+const cache = require('../middleware/redisMidlleware');
 const router = express.Router();
-router.get("/",getAllClassrooms);
+router.get("/",cache(600),getAllClassrooms);
 router.get("/:id", getClassroomById);
 module.exports = router;
