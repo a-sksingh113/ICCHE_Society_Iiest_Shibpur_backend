05cea2080d1a0dffe7937af76600262088c3d5aa
Satish Singh <i.sksingh113@gmail.com>
2025-04-24 04:55:00 +0530

    redis

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { getAllStudents, getStudentById} = require("../controllers/studentController");
+const cache = require('../middleware/redisMidlleware');
 const router = express.Router();
-router.get("/", getAllStudents);
+router.get("/", cache(600),getAllStudents);
 router.get("/:id", getStudentById);
 module.exports = router;
 
