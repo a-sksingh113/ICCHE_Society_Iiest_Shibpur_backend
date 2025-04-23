ed2ae11c046e36ad8f8cda0ad3235db3c73eec85
Satish Singh <i.sksingh113@gmail.com>
2025-04-24 04:47:59 +0530

    redis implemented

--- backend/routes/homeImageRoute.js
+++ backend/routes/homeImageRoute.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const cache = require('../middleware/redisMidlleware');
 const { getHomePageImage } = require("../controllers/homePageImageController");
 
-router.get('/',getHomePageImage);
+
+router.get('/',cache(600),getHomePageImage);
 
 
 module.exports = router;
